a7e2c83d2f92935cdfa135acfefd067c1bce394d
LilCode <lilcode2704@gmail.com>
2026-05-06 15:58:31 +0700

    fix setups

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://fikrulhanif.github.io", // WAJIB untuk GitHub Pages
+  base: "/", // karena pakai repo username.github.io
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
